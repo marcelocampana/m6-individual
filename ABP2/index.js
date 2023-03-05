5f6dc97f5b4584ecc75a0af6b857e650c41cc331
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-03-04 21:48:10 -0300

    finalizado!

--- ABP2/index.js
+++ ABP2/index.js
@@ -1,33 +1,39 @@
+// Este código importa la librería Express.js e inicializa una aplicación Express.
 const express = require("express");
 const app = express();
+
+// Este código importa dos arrays con datos sobre animales del zodiaco chino y signos del zodiaco occidental.
 const zodiacAnimals = require("./zodiacAnimals");
 const zodiacSigns = require("./zodiacSigns");
 
+// Este código establece una ruta para la página de inicio, que simplemente envía la cadena "home" como respuesta.
 app.get("/", (req, res) => {
   res.send("home");
 });
-app.get("/astrologia-china", (req, res) => {
-  const year = parseInt(req.query.date.slice(0, 4));
+
+// Este código establece una ruta para la página de astrología china, que toma un año como parámetro y devuelve el animal del zodiaco correspondiente y su descripción.
+app.get("/astrologia-china/:date", (req, res) => {
+  const year = parseInt(req.params.date.slice(0, 4));
   const animal = zodiacAnimals.find((element) => element.dates.includes(year));
-  res.send(
-    `Tú animal correspondiente al zodíaco chino es <strong>${animal.animal}<strong>`
-  );
+  res.send(`<h1>Eres ${animal.animal}!</h1>
+            <div>${animal.description}</div>`);
 });
-app.get("/zodiaco", (req, res) => {
-  const date = parseInt(req.query.date.slice(4));
-  console.log(zodiacSigns);
+
+// Este código establece una ruta para la página del zodiaco occidental, que toma una fecha en formato MMDD y devuelve el signo del zodiaco correspondiente y su descripción.
+app.get("/zodiaco/:date", (req, res) => {
+  const date = parseInt(req.params.date.slice(4));
   const index = zodiacSigns.findIndex(
     (element) => element.start <= date && element.end >= date
   );
-
-  console.log(index);
-
-  res.send(`Tu signo es ${zodiacSigns[index].zodiacSign}`);
+  res.send(`<h1>Tu signo es ${zodiacSigns[index].zodiacSign}</h1>
+            <div>${zodiacSigns[index].description}</div>`);
 });
+// Este código establece una ruta para todas las demás páginas que no tienen una ruta definida, que devuelve un mensaje de error.
 app.get("*", (req, res) => {
   res.status(400).send("Página no encontrada :(");
 });
 
+// Este código inicia el servidor en el puerto 3000 y muestra un mensaje en la consola indicando que el servidor ha comenzado.
 app.listen("3000", () => {
-  console.log("server start!");
+  console.log("¡Servidor iniciado!");
 });
--- ABP2/zodiacSigns.js
+++ ABP2/zodiacSigns.js
@@ -1,15 +1,87 @@
 const zodiacSigns = [
-  { zodiacSign: "aries", start: 321, end: 419 },
-  { zodiacSign: "tauro", start: 420, end: 520 },
-  { zodiacSign: "géminis", start: 521, end: 620 },
-  { zodiacSign: "cáncer", start: 621, end: 722 },
-  { zodiacSign: "leo", start: 723, end: 822 },
-  { zodiacSign: "virgo", start: 823, end: 922 },
-  { zodiacSign: "libra", start: 923, end: 1022 },
-  { zodiacSign: "escorpio", start: 1023, end: 1121 },
-  { zodiacSign: "sagitario", start: 1122, end: 1221 },
-  { zodiacSign: "capricornio", start: 1222, end: 119 },
-  { zodiacSign: "acuario", start: 120, end: 218 },
-  { zodiacSign: "piscis", start: 219, end: 320 },
+  {
+    zodiacSign: "Aries",
+    start: 321,
+    end: 419,
+    description:
+      "Los arianos son enérgicos, apasionados y creativos. Se dice que son líderes naturales y que tienen un gran potencial para el éxito.",
+  },
+  {
+    zodiacSign: "Tauro",
+    start: 420,
+    end: 520,
+    description:
+      "Los taurinos son prácticos, estables y confiables. Se dice que tienen una gran fuerza interior y que pueden ser muy perseverantes cuando se trata de alcanzar sus objetivos.",
+  },
+  {
+    zodiacSign: "Géminis",
+    start: 521,
+    end: 620,
+    description:
+      "Los geminianos son intelectuales, comunicativos y sociales. Se dice que son muy curiosos y que tienen un gran sentido del humor.",
+  },
+  {
+    zodiacSign: "Cáncer",
+    start: 621,
+    end: 722,
+    description:
+      "Los cancerianos son emocionales, intuitivos y sensibles. Se dice que son muy leales y que tienen un gran corazón.",
+  },
+  {
+    zodiacSign: "Leo",
+    start: 723,
+    end: 822,
+    description:
+      "Los leoninos son enérgicos, apasionados y creativos. Se dice que son líderes naturales y que tienen un gran sentido de la justicia.",
+  },
+  {
+    zodiacSign: "Virgo",
+    start: 823,
+    end: 922,
+    description:
+      "Los virginianos son prácticos, estables y confiables. Se dice que tienen una gran atención al detalle y que pueden ser muy perfeccionistas.",
+  },
+  {
+    zodiacSign: "Libra",
+    start: 923,
+    end: 1022,
+    description:
+      "Los librianos son intelectuales, comunicativos y sociales. Se dice que tienen un gran sentido de la armonía y que pueden ser muy diplomáticos.",
+  },
+  {
+    zodiacSign: "Escorpio",
+    start: 1023,
+    end: 1121,
+    description:
+      "Los escorpianos son emocionales, intuitivos y sensibles. Se dice que tienen una gran fuerza interior y que pueden ser muy apasionados.",
+  },
+  {
+    zodiacSign: "Sagitario",
+    start: 1122,
+    end: 1221,
+    description:
+      "Los sagitarianos son enérgicos, apasionados y creativos. Se dice que son aventureros y que tienen un gran sentido del humor.",
+  },
+  {
+    zodiacSign: "Capricornio",
+    start: 1222,
+    end: 119,
+    description:
+      "Los capricornianos son prácticos, estables y confiables. Se dice que son muy ambiciosos y que tienen un gran sentido de la responsabilidad.",
+  },
+  {
+    zodiacSign: "Acuario",
+    start: 120,
+    end: 218,
+    description:
+      "Los acuarianos son intelectuales, comunicativos y sociales. Se dice que son muy innovadores y que tienen un gran espíritu humanitario.",
+  },
+  {
+    zodiacSign: "Piscis",
+    start: 219,
+    end: 320,
+    description:
+      "Los piscianos son emocionales, intuitivos y sensibles. Se dice que son muy compasivos y que tienen una gran capacidad de empatía.",
+  },
 ];
 module.exports = zodiacSigns;
