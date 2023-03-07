5d6c30fbbd1dcd002be8515e0d5b932bd8241b75
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-03-07 09:56:00 -0300

    apb3 finalizado

--- ABP3/app.js
+++ ABP3/app.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
+const path = require("path");
+
+//Establecer la ubicacion de los partials
+const partialsDir = path.join(__dirname, "views", "partials");
+// Registrar todas las partials en el directorio
+hbs.registerPartials(partialsDir);
 
 const countries = require("./countries.js");
-
+const data = countries
+  .map((element) => {
+    const valores = Object.values(element);
+    return valores.map((value) => value);
+  })
+  .flat();
+//hbs.registerPartial("card", "{{card}}");
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  res.render(
-    "index",
-    { countries }
-    /*    countries.forEach((element) => {
-      for (let clave in element) {
-        console.log(element[clave].Continente);
-        return {
-          continente: element[clave].Continente,
-        };
-      }
-    })*/
-  );
+  res.render("index", { data });
 });
 
 app.listen(3000, () => {
